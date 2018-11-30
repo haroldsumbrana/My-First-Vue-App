4375dbaab387a3dfd133583ec17e7704c1f5601f
Harold Sumbrana <harbatdays@gmail.com>
2018-11-30 22:39:42 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+	<header class="row">
+		<nav class="navbar navbar-toggleable-md navbar-light">
+			<a href="/" class="navbar-brand">PIZZA PLANET</a>
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="">home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="">menu</a>
+				</li>
+				
+				
+			</ul>
+		</nav>
+
+	</header>
+
+</template>
